a74de8c66c03bdc2ffa61d3f9bd44c093ede248b
Kazuo Koga <obiwanko@me.com>
2015-05-23 11:52:16 +0900

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to version-string
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/version_string/core_test.clj
@@ -0,0 +1,7 @@
+(ns version-string.core-test
+  (:require [clojure.test :refer :all]
+            [version-string.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
